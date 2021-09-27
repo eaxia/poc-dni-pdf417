f9b18d0b0de3d01465bc0799c16fd9c2a4aae333
eaxia <91505910+eaxia@users.noreply.github.com>
2021-09-27 20:44:10 -0300

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,7 @@
 const path                 = require('path');
 const HtmlWebpackPlugin    = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CleanWebpackPlugin   = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
